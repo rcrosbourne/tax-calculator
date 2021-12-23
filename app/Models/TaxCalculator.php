8f570a7ffd41f80064c286a8fa2bdd35699c8a41
Rainaldo <rcrosbourne@gmail.com>
2021-12-23 07:21:57 -0500

    NIS calculation for an Individual

---
+++ app/Models/TaxCalculator.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Money\Currencies\ISOCurrencies;
+use Money\Currency;
+use Money\Formatter\DecimalMoneyFormatter;
+use Money\Money;
+use Money\Parser\DecimalMoneyParser;
+
+class TaxCalculator
+{
+    private Money $monthlyGrossAsMoney;
+    private Money $nisAnnualIncomeThresholdAsMoney;
+    private DecimalMoneyParser $moneyParser;
+
+    public function __construct(
+        public string $monthlyGross,
+        public string $nisPercent,
+        public string $nisAnnualIncomeThreshold,
+        public ?Currency $currency = NULL
+    ) {
+        $this->moneyParser = new DecimalMoneyParser(new ISOCurrencies());
+        $this->currency = $this->currency ?? new Currency('JMD');
+
+        // Convert the value 3% to 0.03
+        $this->nisPercent = $this->nisPercent / 100;
+        $this->monthlyGrossAsMoney = $this->moneyParser->parse($this->monthlyGross, $this->currency);
+        $this->nisAnnualIncomeThresholdAsMoney = $this->moneyParser->parse($this->nisAnnualIncomeThreshold,
+            $this->currency);
+    }
+
+    public static function formatAsString(Money $money): string
+    {
+        $formatter = new DecimalMoneyFormatter(new ISOCurrencies());
+        return $formatter->format($money);
+    }
+
+
+
+    /**
+     * @return Money
+     */
+    public function nisAmount(): Money
+    {
+        $nisMaxMonthlyAmountBasedOnIncomeThreshold = $this->nisMaxYearlyAmountBasedOnIncomeThreshold()->divide('12');
+        $nisAmountUsingPercentageOfMonthlyGross = $this->monthlyGrossAsMoney->multiply($this->nisPercent);
+        return min($nisMaxMonthlyAmountBasedOnIncomeThreshold, $nisAmountUsingPercentageOfMonthlyGross);
+    }
+
+    /**
+     * @return Money
+     */
+    private function nisMaxYearlyAmountBasedOnIncomeThreshold(): Money
+    {
+        return $this->nisAnnualIncomeThresholdAsMoney->multiply($this->nisPercent);
+    }
+}
---
+++ tests/Unit/TaxCalculatorUnitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\TaxCalculator;
+use Money\Money;
+use Tests\TestCase;
+
+
+class TaxCalculatorUnitTest extends TestCase
+{
+    /** @test */
+    public function it_can_calculate_nis()
+    {
+        $calculator = new TaxCalculator(
+            monthlyGross: '100000.00',
+            nisPercent: '3',
+            nisAnnualIncomeThreshold: '3000000.00'
+        );
+
+        $nisAmount = $calculator->nisAmount();
+
+        $this->assertEquals('3000.00', TaxCalculator::formatAsString($nisAmount));
+    }
+
+    /** @test */
+    public function it_calculates_nis_respecting_income_threshold()
+    {
+        $calculator = new TaxCalculator(
+            monthlyGross: '264750.00',
+            nisPercent: '3',
+            nisAnnualIncomeThreshold: '3000000.00'
+        );
+
+        $nisAmount = $calculator->nisAmount();
+        $this->assertEquals('7500.00', TaxCalculator::formatAsString($nisAmount));
+    }
+}
