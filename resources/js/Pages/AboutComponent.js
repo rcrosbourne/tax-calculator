d1c6194d28d92af4292bde766017c8870adca9c7
Rainaldo <rcrosbourne@gmail.com>
2021-12-25 16:18:40 -0500

    Added some snippets from tailwindui

--- resources/js/Pages/AboutComponent.js
+++ resources/js/Pages/AboutComponent.js
@@ -70,7 +70,7 @@ export default function AboutComponent() {
         </svg>
 
         <div className="relative">
-          <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl text-red-400">
             A better way to send money
           </h2>
           <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
