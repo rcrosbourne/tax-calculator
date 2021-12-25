e9a1f0ed82bf9a1f0e3dca720a446040c9905dfe
Rainaldo <rcrosbourne@gmail.com>
2021-12-25 16:01:18 -0500

    Installed Inertia

---
+++ resources/js/Pages/AboutComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Layout from './Layout'
+import { Head } from '@inertiajs/inertia-react'
+
+export default function AboutComponent() {
+  return (
+    <div>
+      <Head title="About" />
+      <h1>Welcome</h1>
+      <p>Hello Ziggy, welcome to your first Inertia app!</p>
+    </div>
+  )
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Tax Calculator</title>
+
+    <!-- Fonts -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <!-- Styles -->
+</head>
+<body>
+@inertia
+</body>
+</html>
