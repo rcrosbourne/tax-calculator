025fbb6571b0eaa78b498ca5f7d6b0e5f9a1691f
Rainaldo <rcrosbourne@gmail.com>
2021-12-25 02:15:11 -0500

    Added NHT seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(NISSeeder::class);
+        $this->call(NHTSeeder::class);
         $this->call(EducationTaxSeeder::class);
     }
 }
