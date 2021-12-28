cc2d2a4b7558583d5002860b08036b87dfa12ff0
Rainaldo <rcrosbourne@gmail.com>
2021-12-27 21:58:17 -0500

    updated readme

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::post('calculate', [ApiTaxCalculatorController::class, 'store']);
